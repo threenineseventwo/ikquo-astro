8015d6d46a5d36f4cd2ec366c8d27cfcbf71a9e2
threenineseventwo <alvinlee020706@gmail.com>
2023-11-28 15:54:47 -0500

    Made the RSS feed work

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-export async function get (context) {
+export async function GET(context) {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
